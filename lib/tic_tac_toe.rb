1cacd7383800dfd407d0dbd47a4c3979f517468e
Emilia Ziemichód <emifrompoland@gmail.com>
2019-01-08 03:12:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -119,7 +119,7 @@ def play(board)
  end
  if won?(board)
    winner(board)=="X" || winner(board)=="O"
-   puts"Congratulations!"
+   puts"Congratulations #{winner}!"
  elsif draw?(board)
    puts""
  end
