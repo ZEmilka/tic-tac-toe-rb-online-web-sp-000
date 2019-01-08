3fb6494974c1f3e76cc6ee3e95f14945b62a946e
Emilia Ziemichód <emifrompoland@gmail.com>
2019-01-08 03:02:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,8 +34,8 @@ def turn(board)
   puts"Please enter 1-9:"
   input=gets.strip
   index=input_to_index(input)
-  if valid_move?(board,current_player(board))
-    move(board,index,token)
+  if valid_move?(board,index)
+    move(board,index,current_player(board))
     display_board(board)
   else
     turn(board)
